e45524137c6a8ca619cbd9ec7c0f97b660364671
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-15 00:36:47 -0300

    Added Routes.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,54 @@
-function App() {
+import React from 'react'
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
+
+const App = () => {
 	return (
-		<div className="App">
-			<h1>App</h1>
-		</div>
+		<BrowserRouter>
+			<div>
+				<nav>
+					<ul>
+						<li>
+							<Link to="/">Home</Link>
+						</li>
+						<li>
+							<Link to="/sign-in">Sign-in</Link>
+						</li>
+						<li>
+							<Link to="/sign-up">Sign-up</Link>
+						</li>
+						<li>
+							<Link to="/manage/places/create">Create Place</Link>
+						</li>
+						<li>
+							<Link to="/manage/places/edit">Edit Place</Link>
+						</li>
+						<li>
+							<Link to="/manage/places">Places</Link>
+						</li>
+					</ul>
+				</nav>
+				<Switch>
+					<Route exact path="/">
+						<h1>Home</h1>
+					</Route>
+					<Route path="/sign-in">
+						<h1>Sign-in</h1>
+					</Route>
+					<Route path="/sign-up">
+						<h1>Sign-up</h1>
+					</Route>
+				</Switch>
+				<Route exact path="/manage/places/create">
+					<h1>Create Place</h1>
+				</Route>
+				<Route exact path="/manage/places/edit">
+					<h1>Edit Place</h1>
+				</Route>
+				<Route exact path="/manage/places">
+					<h1>Places</h1>
+				</Route>
+			</div>
+		</BrowserRouter>
 	)
 }
 
