3bfa8a92604a6dada16bbfe2ad46afa7002a3d0b
Pedro Giampietro <pedrogiampietro@hotmail.com>
2020-12-15 17:44:31 -0300

    Added UI Kit c @Createive

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import './assets/css/bootstrap.min.css'
+import './assets/css/now-ui-kit.min.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
